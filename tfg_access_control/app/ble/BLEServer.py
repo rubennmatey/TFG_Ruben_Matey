a5a282cddf19b09047a48983bde328a34473facb
rubennmatey <rubenmatey14@gmail.com>
2026-04-20 16:04:35 +0200

    Changes in tests nfc + ble

--- tfg_access_control/app/ble/BLEServer.py
+++ tfg_access_control/app/ble/BLEServer.py
@@ -1,4 +1,4 @@
-from bluezero import peripheral, async_tools
+from bluezero import peripheral
 
 ADAPTER_ADDRESS = "2C:CF:67:E5:C9:88"
 
@@ -27,10 +27,8 @@ class BLEServer:
             uuid=CHAR_UUID,
             value=[],
             notifying=False,
-            flags=["read", "notify"],
-            read_callback=self.read_value,
-            write_callback=None,
-            notify_callback=self.notify_callback
+            flags=["read"],
+            read_callback=self.read_value
         )
 
     def read_value(self):
@@ -40,16 +38,6 @@ class BLEServer:
     def update_value(self, new_value: str):
         self.current_value = new_value
         print(f"[BLE] Valor actualizado a: {self.current_value}")
-
-    def _notify_update(self, characteristic):
-        print(f"[BLE] Enviando notify con valor: {self.current_value}")
-        characteristic.set_value(list(self.current_value.encode("utf-8")))
-        return characteristic.is_notifying
-
-    def notify_callback(self, notifying, characteristic):
-        print(f"[BLE] notify_callback llamado. notifying={notifying}")
-        if notifying:
-            async_tools.add_timer_seconds(1, self._notify_update, characteristic)
 
     def start(self):
         print("BLE iniciado")
